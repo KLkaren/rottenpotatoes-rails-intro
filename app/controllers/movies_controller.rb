6162ed35e0d1d3169fbdba3b26380293100d3fbf
KLkaren <karen_xkl@tamu.edu>
2019-02-16 00:48:32 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,9 @@
 class MoviesController < ApplicationController
-
+  
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
-
+  
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -11,27 +11,53 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @all_ratings = Movie.all.sort_by{|x| x[:rating]}.map{|x| x.rating}.uniq  # find the rating list by consulting the model
+    redirect = false  # determine if the page needs to be redirected
     
-    @all_ratings = Movie.all.sort_by{|x| x[:rating]}.map{|x| x.rating}.uniq
-    @selected_ratings = with_ratings
-    @selected_ratings.each do |rating|
-        params[:rating] = true
-    end
-    
+    # Use seesion to store passed parameter
     if params[:sort]
-      @movies = Movie.order(params[:sort])
+      @sort = params[:sort]
+      session[:sort] = params[:sort]
+    elsif session[:sort]
+      @sort = session[:sort]
+      redirect = true
     else
-      @movies = Movie.where(:rating => @selected_ratings)
+      @sort = nil
     end
-  end
-  
-  # Takes an array pf ratings and return matched ones
-  def with_ratings
-    if params[:ratings]
-      params[:ratings].keys
+      
+    if params[:commit]== "Refresh" and params[:ratings].nil?
+      @ratings = nil
+      session[:ratings] = nil
+    elsif params[:ratings]
+      @ratings = params[:ratings]
+      session[:ratings] = params[:ratings]
+    elsif session[:ratings]
+      @ratings = session[:ratings]
+      redirect = true
+    end
+      
+    # Get the redirect page
+    if redirect
+      flash.keep
+      redirect_to movies_path :sort=> @sort, :ratings=> @ratings
+    end
+    
+    # If none of the box is checked, return the page with no results
+    if @ratings.nil?
+      @ratings = Hash.new
+    end
+    
+    # Display the page according to the filter and sort options
+    if @sort and @ratings
+       @movies = Movie.where(:rating => @ratings.keys).order(@sort)
+    elsif @sort
+       @movies = Movie.order(@sort)
+    elsif @ratings
+       @movies = Movie.where(:rating => @ratings.keys)
     else
-      @all_ratings
+       @movies = Movie.all
     end
+    
   end
 
   def new
